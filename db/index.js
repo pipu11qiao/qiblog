bb4dfcaa21650e2aa0210fe24451c425ac1f0b44
pipu11qiao <18854558196@163.com>
2017-06-27 10:17:29 +0800

    user

--- db/index.js
+++ db/index.js
@@ -1,23 +1,32 @@
-
-var mongoose=require('mongoose');
-mongoose.Promise=Promise;
+var mongoose = require('mongoose');
+mongoose.Promise = Promise;
 mongoose.connect('mongodb://127.0.0.1/qiblog');
-var ObjectId=mongoose.Schema.Types.Object;
+var ObjectId = mongoose.Schema.Types.Object;
 // 用户模块
-var UserSchema=new mongoose.Schema({
-    username:String,
-    password:String,
-    email:String,
-    avatar:String,
-    gender: String
+var UserSchema = new mongoose.Schema({
+	username: String,
+	password: String,
+	email: String,
+	avatar: String,
+	gender: String,
+	createAt: {
+		type: Date,
+		default: Date.now
+	}
 });
-exports.User=mongoose.model('User',UserSchema);
+exports.User = mongoose.model('User', UserSchema);
 
 // 文章模块
-var ArticleSchema=new mongoose.Schema({
-    title:String,
-    content:{type:String},
-    createAt:{type:Date,default:new Date},
-    user:{type:ObjectId,ref:'User'}
+var ArticleSchema = new mongoose.Schema({
+	title: String,
+	content: {type: String},
+	createAt: {
+		type: Date,
+		default: new Date
+	},
+	user: {
+		type: ObjectId,
+		ref: 'User'
+	}
 });
-exports.Article=mongoose.model('Article',ArticleSchema);
+exports.Article = mongoose.model('Article', ArticleSchema);
