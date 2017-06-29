c8ad7d30d1f0c0e47d889f9c2318832070618aa0
pipu11qiao <18854558196@163.com>
2017-06-29 15:08:25 +0800

    message list

--- routes/message.js
+++ routes/message.js
@@ -7,7 +7,7 @@ var router = express.Router();
 var util = require('../util');
 var Send = util.Send;
 
-//文章增加
+//留言增加
 router.post('/add', checkLogin, function (req, res) {
   //取得请求体对象
   var user = req.session.user;
@@ -22,6 +22,43 @@ router.post('/add', checkLogin, function (req, res) {
       res.send(Send.s2(doc));
     }
   })
+});
+//文章列表
+router.post('/index', function (req, res) {
+	var pageSize = Math.floor(req.body.pageSize);
+	//取得请求体对象
+	Message.find({}).sort({createTime: -1}).limit(pageSize).populate('user').exec(function (err, messages) {
+		if (err) {
+			res.send(Send.s5(err));
+		} else {
+			res.send(Send.s2({
+				messages: messages,
+				pageSize: pageSize,
+			}));
+		}
+	});
+});
+//文章列表
+router.post('/list', function (req, res) {
+	var pageNum = Math.floor(req.body.pageNum);
+	var pageSize = Math.floor(req.body.pageSize);
+	//取得请求体对象
+	Message.find({}).sort({createTime: -1}).skip((pageNum - 1) * pageSize).limit(pageSize).populate('user').exec(function (err, messages) {
+	  console.log(messages);
+		Message.count({}, function (err, count) {
+		  console.log(messages);
+			if (err) {
+				res.send(Send.s5(err));
+			} else {
+				res.send(Send.s2({
+					messages: messages,
+					pageSize: pageSize,
+					pageNum: pageNum,
+					count: count
+				}));
+			}
+		});
+	});
 });
 
 module.exports = router;
